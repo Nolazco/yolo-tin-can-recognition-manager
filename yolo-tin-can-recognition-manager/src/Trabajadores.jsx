65607632e1084dd044b3713e6ddd131a037b67ac
Carlos Nolazco <carlos_sceptile@hotmail.com>
2023-03-14 09:10:52 -0600

    first layout

---
+++ yolo-tin-can-recognition-manager/src/Trabajadores.jsx
@@ -0,0 +1,41 @@
+import Users from './data/users.json';
+import Table from 'react-bootstrap/Table';
+import Nav from './Nav';
+
+function Trabajadores(){
+	return(
+		<>
+			<Nav/>
+			<center>
+			<div style={{ width: '80%' }}>
+				<Table striped bordered>
+			      <thead>
+			        <tr>
+			          <th>#</th>
+			          <th>Nombre</th>
+			          <th>Apellido paterno</th>
+			          <th>Apellido materno</th>
+			          <th>Email</th>
+			        </tr>
+			      </thead>
+			      <tbody>
+			      {Users.map(user => {
+			      	return(
+			      	<tr key={user.id}>
+			      	  <td>{user.id}</td>
+			          <td>{user.nombre}</td>
+			          <td>{user.aPaterno}</td>
+			          <td>{user.aMaterno}</td>
+			          <td>{user.correo}</td>
+			        </tr>
+			      	)
+			      })}
+			      </tbody>
+			    </Table>
+		    </div>
+		    </center>
+		</>
+	);
+}
+
+export default Trabajadores;
