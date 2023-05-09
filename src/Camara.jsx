220ab21239935d373ba6a2532a4384e851bf81f6
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-05-08 22:16:43 -0600

    avance acomodo simplificado

--- src/Camara.jsx
+++ src/Camara.jsx
@@ -1,4 +1,37 @@
+import * as React from 'react';
 import {useRef, useEffect, useCallback} from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import FormControl from '@mui/material/FormControl';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import TinmanLogo from './TinmanLogo';
+import { Form } from 'react-router-dom';
+import InputLabel from '@mui/material/InputLabel';
+
+function createData(
+  Name: string,
+  Wrong: number,
+  Right: number,
+  Total: number,
+) {
+  return { Name, Wrong, Right, Total };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
 
 function Camara(){
 	const vid = useRef(null);
@@ -12,19 +45,67 @@ function Camara(){
 	});
 
 	return(
-		<>
-			<div className="row">
-				<div className="col-md-6">
-					<video autoPlay ref={vid}/>
-				</div>
-				<div className="col-md-6">
-					<h2>Latas en buen estado: #</h2><br/>
-					<h2>Latas en mal estado: #</h2><br/>
-					<h2>Latas totales: #</h2><br/>
-				</div>
-			</div>
-		</>
-	);
-}
+	<>
+		<Form method="post" action="/" style={{
+		    'width': '500px',
+		    'margin': 'auto',
+		    'marginTop': '2em',
+		    'padding': '10px'
+		}}>
+		<Paper elevation={3}>
+		    <Card variant="outlined">
+		        <CardContent center={true}>
+		            <TinmanLogo size={200} center={true}/>
+		        </CardContent>
+		    </Card>
+		</Paper>
+		</Form>
 
+
+
+	<div class="esp-cam">
+		<video class="camara" autoPlay ref={vid}/>
+	</div>
+	<BasicTable />
+
+	</>
+
+	);
+
+}
 export default Camara;
+
+
+function BasicTable() {
+  return (
+  	<Paper elevation={3}>
+	    <TableContainer component={Paper}>
+	      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+	        <TableHead>
+	          <TableRow>
+	            <TableCell>Empleado/a</TableCell>
+	            <TableCell align="right">Wrong</TableCell>
+	            <TableCell align="right">Right&nbsp;(g)</TableCell>
+	            <TableCell align="right">Total&nbsp;(g)</TableCell>
+	          </TableRow>
+	        </TableHead>
+	        <TableBody>
+	          {rows.map((row) => (
+	            <TableRow
+	              key={row.Name}
+	              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+	            >
+	              <TableCell component="th" scope="row">
+	                {row.Name}
+	              </TableCell>
+	              <TableCell align="right">{row.Wrong}</TableCell>
+	              <TableCell align="right">{row.Right}</TableCell>
+	              <TableCell align="right">{row.Total}</TableCell>
+	            </TableRow>
+	          ))}
+	        </TableBody>
+	      </Table>
+	    </TableContainer>
+	</Paper>
+  );
+}
