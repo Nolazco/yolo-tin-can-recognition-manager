659fe1338cafa2df817b9da2925a2f0690c27bcc
Roberto <111545434+Jamiro-MR@users.noreply.github.com>
2023-06-04 15:15:40 -0600

    Small change
    -Legend con Victory
    -Acomodo un poco de las graficas
    -Texto

--- src/Graficos.jsx
+++ src/Graficos.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { VictoryPie, VictoryLabel, VictoryBar, VictoryChart, VictoryHistogram, VictoryAxis, VictoryPortal } from 'victory';
+import { VictoryPie, VictoryLabel, VictoryBar, VictoryChart, VictoryHistogram, VictoryAxis, VictoryPortal, VictoryLegend } from 'victory';
 import { useEffect, useState } from 'react';
 import firebase from './firebase';
+import './App.css'; // Import the CSS file
 
-function Graficos() { //Datos placeholder, cambiar a discreción
+function Graficos() {
   const [buenasData, setBuenasData] = useState([]);
   const [malasData, setMalasData] = useState([]);
   const [ultimoPromedioBuenas, setUltimoPromedioBuenas] = useState(0);
@@ -23,7 +24,6 @@ function Graficos() { //Datos placeholder, cambiar a discreción
       }
     });
 
-    // Obtener los datos de "malas"
     const malasRef = database.ref('malas');
     malasRef.on('value', (snapshot) => {
       const data = snapshot.val();
@@ -37,12 +37,12 @@ function Graficos() { //Datos placeholder, cambiar a discreción
   }, []);
 
   const ultimasBuenas = buenasData.slice(Math.max(buenasData.length - 5, 0));
-
   const ultimasMalas = malasData.slice(Math.max(malasData.length - 5, 0));
 
   return (
     <div className="graficos-container">
       <div className="chart-container">
+        <div className="chart-text chart-text-highlight">Ultimo periodo detectado - Piechart</div>
         <VictoryChart height={300} width={400}>
           <VictoryPie
             data={[
@@ -64,46 +64,65 @@ function Graficos() { //Datos placeholder, cambiar a discreción
         </VictoryChart>
       </div>
       <div className="chart-container">
-        <VictoryChart height={300} width={400} style={{ backgroundColor: 'white', parent: { backgroundColor: 'white' } }}>
-          <VictoryAxis style={{
-              axisLabel: { fill: 'black' },
-              tickLabels: { fill: 'black' },
-              padding: 40,
-            }} dependentAxis domain={[0, 100]} tickValues={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} />
-          <VictoryBar
-            data={ultimasBuenas.map((item, index) => ({
-              x: index,
-              y: item.promedio,
-            }))}
-            style={{ data: { fill: 'blue' } }}
-            barWidth={15} // Ajusta el valor según tus necesidades
-          />
-          <VictoryBar
-            data={ultimasMalas.map((item, index) => ({
-              x: index + 0.3,
-              y: item.promedio,
-            }))}
-            style={{ data: { fill: 'red' } }}
-            barWidth={15} // Ajusta el valor según tus necesidades
-          />
-          <VictoryAxis
-            style={{
-              axis: { stroke: 'gray' },
-              ticks: { stroke: 'white'},
-              tickLabels: { fill: 'white' },
-              axisLabel: { fill: 'white' },
-            }}
-          />
-          <VictoryAxis
-            dependentAxis
-            label="Promedio"
-            style={{
-              axis: { stroke: 'gray' },
-              ticks: { stroke: 'gray'},
-              axisLabel: { padding: 30 },
-            }}
-          />
-        </VictoryChart>
+        <div className="chart-text chart-text-highlight">Volumen - Historigram</div>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <VictoryChart height={300} width={400} style={{ backgroundColor: 'white', parent: { backgroundColor: 'white' } }}>
+            <VictoryAxis
+              style={{
+                axisLabel: { fill: 'black' },
+                tickLabels: { fill: 'black' },
+                padding: 40,
+              }}
+              dependentAxis
+              domain={[0, 100]}
+              tickValues={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]}
+            />
+            <VictoryBar
+              data={ultimasBuenas.map((item, index) => ({
+                x: index,
+                y: item.promedio,
+              }))}
+              style={{ data: { fill: 'blue' } }}
+              barWidth={15}
+            />
+            <VictoryBar
+              data={ultimasMalas.map((item, index) => ({
+                x: index + 0.3,
+                y: item.promedio,
+              }))}
+              style={{ data: { fill: 'red' } }}
+              barWidth={15}
+            />
+            <VictoryAxis
+              style={{
+                axis: { stroke: 'gray' },
+                ticks: { stroke: 'white' },
+                tickLabels: { fill: 'white' },
+                axisLabel: { fill: 'white' },
+              }}
+            />
+            <VictoryLegend
+              x={120}
+              y={266}
+              orientation="horizontal"
+              gutter={20}
+              style={ { border: { stroke: "black" } } }
+              data={[
+                { name: "Malas", symbol: { fill: "red", type: "star" } },
+                { name: "Buenas", symbol: { fill: "blue", type: "star" } }
+              ]}
+            />
+            <VictoryAxis
+              dependentAxis
+              label="Promedio"
+              style={{
+                axis: { stroke: 'gray' },
+                ticks: { stroke: 'gray' },
+                axisLabel: { padding: 30 },
+              }}
+            />
+          </VictoryChart>
+        </div>
       </div>
     </div>
   );
